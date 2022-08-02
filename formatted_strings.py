a504e85ec4ba5647f30af08e863bd7ced249674b
sufiyan <90792596+suf01@users.noreply.github.com>
2022-08-02 14:52:01 +0530

    Add files via upload

---
+++ formatted_strings.py
@@ -0,0 +1,16 @@
+# method 1
+print("This is a formated {}".format('string'))
+
+# method 2
+print("this is {1} and the {0} words".format('last', 'first'))
+
+# method 3
+print("this a {a} formated {b}".format(a = "another", b = "string"))
+
+# method 4
+a = 1
+b = 2
+print(f"a = {a} and b = {b}")
+
+
+
